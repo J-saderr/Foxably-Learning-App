89047e6c39972361c815d123a36d2de0ad2a3039
Sleepapp <vontthao@gmail.com>
2024-03-28 21:24:36 +0700

    Change scene

---
+++ src/main/java/com/example/foxably/MainController.java
@@ -0,0 +1,67 @@
+package com.example.foxably;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MainController {
+    private Stage stage;
+    private Scene scene;
+    public Connection connect;
+    public PreparedStatement prepare;
+    public ResultSet result;
+
+    public static Connection connectDb(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/Book", "root", "pass"); // address, database username, database password
+            return connect;
+        }catch(Exception e){e.printStackTrace();}
+        return null;
+    }
+    public void switchToChat(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/foxably/chat-view.fxml"));
+        scene = new Scene(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToCourse(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/foxably/course-view.fxml"));
+            scene = new Scene(root);
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace(); // This will print the stack trace of the exception
+        }
+    }
+
+
+    public void switchToHome(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/foxably/home-view.fxml"));
+        scene = new Scene(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("hi");
+    }
+
+    public void switchToTodolist(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/foxably/content-view.fxml"));
+        scene = new Scene(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
